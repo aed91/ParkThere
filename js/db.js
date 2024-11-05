3f8e78afadadef6b45a39a6b6d273848752df06b
JKrups <jrkrupsky@gmail.com>
2024-11-05 11:12:02 -0500

    removing unnecessary code from db.js

--- js/db.js
+++ js/db.js
@@ -21,30 +21,3 @@ snapshot.docChanges().forEach(change => {
   }
 });
 });
-
-// add new recipe
-const form = document.querySelector('form');
-form.addEventListener('submit', evt => {
-evt.preventDefault();
-
-const recipe = {
-  name: form.title.value,
-  ingredients: form.ingredients.value
-};
-
-db.collection('recipes').add(recipe)
-  .catch(err => console.log(err));
-
-form.title.value = '';
-form.ingredients.value = '';
-});
-
-// remove a recipe
-const recipeContainer = document.querySelector('.recipes');
-recipeContainer.addEventListener('click', evt => {
-if(evt.target.tagName === 'I'){
-  const id = evt.target.getAttribute('data-id');
-  //console.log(id);
-  db.collection('recipes').doc(id).delete();
-}
-})
